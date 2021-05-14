39828e58fc3e1b9a405897e21ad35849ecd83a32
Abdelrahman Lawaty <potashapotas15@gmail.com>
2021-05-14 09:02:20 +0200

    lol

---
+++ BrightTech/platforms/android/app/build/intermediates/merged_assets/debug/mergeDebugAssets/out/www/plugins/microphonePermission/www/cordova-plugin-microphone-permission.js
@@ -0,0 +1,7 @@
+cordova.define("microphonePermission.MicrophonePermission", function(require, exports, module) {
+var exec = require('cordova/exec');
+
+module.exports = microphonePermission: function(command, callback, error) {
+    exec(callback, error, 'Microphone', 'recordPermission', []);
+}
+});
